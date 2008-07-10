6382aa8c78c82c3cb950218ec3988e5da6003063
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2008-07-10 09:26:35 +0000

    Diagnostics concentrator: Add debug option to startup.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/CS/CS-DI-IOC-01@21247 e099a375-04f9-0310-9d5f-a741eaff62e1

--- concentrator.py
+++ concentrator.py
@@ -2,9 +2,20 @@
 
 '''Diagnostics Storage Ring EBPM Concentrator.'''
 
-from pkg_resources import require
-require('cothread==1.7')
-require('dls.builder==1.4')
+import sys
+DEBUG = 'D' in sys.argv[1:]
+if DEBUG:
+    sys.path.append('/scratch/local/python-debug')
+    sys.path.append('/home/mga83/epics/cothread')
+    sys.path.append('/home/mga83/epics/builder/build/lib')
+
+    import cothread
+    def Log(): print 'refs: %8d' % sys.gettotalrefcount()
+    cothread.Timer(1, Log, retrigger = True)
+else:
+    from pkg_resources import require
+    require('cothread==1.7')
+    require('dls.builder==1.4')
 
 import builder
 
